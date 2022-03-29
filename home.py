ea41403ebe44604c0108085cd8afc450c941b930
CatHer431 <catherine.hernandez@sjsu.edu>
2022-03-28 17:29:33 -0700

    test 3

--- home.py
+++ home.py
@@ -2,27 +2,27 @@ from flask import Flask
 
 ques1 = Flask(__name__)
 
-name = 'Cathy'
-city_names = ["Paris", "London", "Rome", "Tahiti"]
+global name
+global city_names
+#city_names = ["Paris", "London", "Rome", "Tahiti"]
 
 @ques1.route("/")
 def home():
  return '''
  <html>
-    <head>
-        <title>Low Pass Question</title>
-    </head>
-    <body>
-        <h1>Welcome {name}!</h1>
-        <a href="https://google.com">not google</a>
-        <ul>
-         <li>{city_names[0]}</li>
-	 <li>{city_names[1]}</li>
-	 <li>{city_names[2]}</li>
-	 <li>{city_names[3]}</li>
-        </ul>
-    </body>
- </html>'''
+ <head>
+ </head>
+  <body>
+   <h1>Welcome """  + name + """ </h1>
+    <a href="www.google.com">not google </a>
+    <ul>
+     {% for city in city_names %}
+      <li>city.name</li>
+     {% endfor %}
+    </ul>
+   </body>
+ </html>""" 
 
-#if __name__ == '__main__':
+
+
 #ques1.run()
