44eb18c823c43c882a9ee3cbc289a8621c71cbdd
CatHer431 <catherine.hernandez@sjsu.edu>
2022-03-28 19:20:39 -0700

    fixing the list print

--- home.py
+++ home.py
@@ -2,30 +2,23 @@ from flask import Flask
 
 ques1 = Flask(__name__)
 
-#global name
-#global city_names
+name = 'Cathy'
+city_names = ["Paris", "London", "Rome", "Tahiti"]
 
 @ques1.route("/")
-def home():
- name = 'Cathy'
- city_names = ["Paris", "London", "Rome", "Tahiti"]
- return '''
- <html>
-    <head>
+def home(): 
+ html = '''
+     <head>
         <title>Low Pass Question</title>
-    </head>
-    <body>
-    <h1>Welcome """  + name + """ </h1>
-    <a href="www.google.com">not google </a>
-    <ul>
-     {% for city in city_names %}
-      <li>city.name</li>
-     {% endfor %}
-    </ul>
-   </body>
- </html>
- ''' 
-
-
+     </head>
+     <body>
+      <h1>Welcome ''' + name + '''! </h1>
+      <a href="www.google.com">not google </a>
+      <ul>
+ '''
+ for city in city_names:
+  html = html + "<li>" + city + "</li>"
+ html = html + '''</ul> </body>'''
+ return html
 
 #ques1.run()
